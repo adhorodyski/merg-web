4ad79694323b52ad2fc6f1a52f8373f155f05e43
Adam Horodyski <adhorodyski@macbook-pro-adam.home>
2020-02-26 23:53:33 +0100

    feature/6 remove api-prefix interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { TokenInterceptor, ApiPrefixInterceptor } from './interceptors';
+import { TokenInterceptor } from './interceptors';
 import { MemberGuard, CreatorGuard, NoAuthGuard } from './guards';
 import { ApiService, ThemeService } from './services';
 
 @NgModule({
     imports: [CommonModule, HttpClientModule, RouterModule],
     providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: ApiPrefixInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
         MemberGuard,
         CreatorGuard,
