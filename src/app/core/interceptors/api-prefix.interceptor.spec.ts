b85f3ec61f91fdc00ca414562111902247088336
Adam Horodyski <adhorodyski@macbook-pro-adam.home>
2020-02-26 18:50:50 +0100

    feature/4 fix tests for interceptors

--- src/app/core/interceptors/api-prefix.interceptor.spec.ts
+++ src/app/core/interceptors/api-prefix.interceptor.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { ApiPrefixInterceptor } from './api-prefix.interceptor';
 
 describe('ApiPrefixInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [ApiPrefixInterceptor],
+    }),
+  );
 
   it('should be created', () => {
     const interceptor: ApiPrefixInterceptor = TestBed.get(ApiPrefixInterceptor);
--- src/app/core/interceptors/token.interceptor.spec.ts
+++ src/app/core/interceptors/token.interceptor.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { TokenInterceptor } from './token.interceptor';
 
 describe('TokenInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [TokenInterceptor],
+    }),
+  );
 
   it('should be created', () => {
     const interceptor: TokenInterceptor = TestBed.get(TokenInterceptor);
