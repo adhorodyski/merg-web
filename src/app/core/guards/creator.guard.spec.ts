498ed75c13abeccd4800310a2ca3c4e9f527432b
Adam Horodyski <adhorodyski@macbook-pro-adam.home>
2020-03-11 11:19:22 +0100

    feature/15 cleanup guards tests

--- src/app/core/guards/creator.guard.spec.ts
+++ src/app/core/guards/creator.guard.spec.ts
@@ -1,9 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { CreatorGuard } from './creator.guard';
-import { AuthService } from '@src/app/core/services/auth.service';
 
 describe('CreatorGuard', () => {
     beforeEach(() => {
@@ -13,11 +11,7 @@ describe('CreatorGuard', () => {
         });
     });
 
-    it('should create an instance', inject(
-        [CreatorGuard, AuthService, Router],
-        (authService: AuthService, router: Router) => {
-            const guard = new CreatorGuard(authService, router);
-            expect(guard).toBeTruthy();
-        },
-    ));
+    it('should create an instance', inject([CreatorGuard], (guard: CreatorGuard) => {
+        expect(guard).toBeTruthy();
+    }));
 });
--- src/app/core/guards/member.guard.spec.ts
+++ src/app/core/guards/member.guard.spec.ts
@@ -1,9 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { MemberGuard } from './member.guard';
-import { AuthService } from '@src/app/core/services/auth.service';
 
 describe('MemberGuard', () => {
     beforeEach(() => {
@@ -13,11 +11,7 @@ describe('MemberGuard', () => {
         });
     });
 
-    it('should create an instance', inject(
-        [MemberGuard, AuthService, Router],
-        (authService: AuthService, router: Router) => {
-            const guard = new MemberGuard(authService, router);
-            expect(guard).toBeTruthy();
-        },
-    ));
+    it('should create an instance', inject([MemberGuard], (guard: MemberGuard) => {
+        expect(guard).toBeTruthy();
+    }));
 });
